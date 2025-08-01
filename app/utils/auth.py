77664914ce0709c17edafb2341114d7e395fb07d
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-08-01 21:20:17 +0530

    auth file

--- app/utils/auth.py
+++ app/utils/auth.py
@@ -7,7 +7,6 @@ from app.utils.jwt import verify_token
 from passlib.context import CryptContext
 import redis
 from typing import Dict
-from app.schemas import UserResponse
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -15,14 +14,15 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 redis_client = redis.StrictRedis(host="localhost", port=6379, db=0, decode_responses=True)
 
 def hash_password(password: str) -> str:
-    """Hash the password using bcrypt."""
     return pwd_context.hash(password)
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
-def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
-
+def get_current_user(
+    token: str = Depends(oauth2_scheme),
+    db: Session = Depends(get_db)
+):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid authentication credentials",
@@ -30,8 +30,12 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     )
 
     try:
-        payload: Dict[str,str] = verify_token(token)
-        user_id :str = payload.get("user_id")
+        payload: Dict = verify_token(token)
+        print("Decoded Payload:", payload)  
+
+        user_id = payload.get("user_id")
+        if not user_id:
+            raise credentials_exception
 
         user = db.query(User).filter(User.id == user_id).first()
 
@@ -43,7 +47,8 @@ def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(
     except HTTPException as e:
         raise e  
 
-    except Exception:
+    except Exception as e:
+        print("Unexpected error:", e)  
         raise credentials_exception
     
 def admin_required(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
@@ -51,7 +56,7 @@ def admin_required(token: str = Depends(oauth2_scheme), db: Session = Depends(ge
     
     user = db.query(User).filter(User.id == payload["user_id"]).first()
 
-    if not user or user.role != UserRole.ADMIN:
+    if not user or user.role != UserRole.ADMIN: # type: ignore
         raise HTTPException(status_code=403, detail="Insufficient permissions")
     
     return user  
