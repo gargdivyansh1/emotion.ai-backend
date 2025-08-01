468b9e5400cfced1def4a4b19a5a6c329b3cb9f9
gargdivyansh1 <divyanshgarg515@gmail.com>
2025-08-01 21:21:07 +0530

    email service

--- app/services/email_serivce.py
+++ app/services/email_serivce.py
@@ -1,6 +1,12 @@
 from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
 from app.config import settings
 from typing import List
+import smtplib
+from email.mime.text import MIMEText
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 conf = ConnectionConfig(
     MAIL_USERNAME=settings.MAIL_USERNAME,
@@ -25,3 +31,15 @@ async def send_email(subject: str, recipients: List[str], body: str, attachments
 
     fm = FastMail(conf)
     await fm.send_message(message)
+    
+
+async def send_email_otp(to_email, message):
+    msg = MIMEText(message)
+    msg["Subject"] = "Your OTP Code"
+    msg["From"] = os.getenv("MAIL_USERNAME")
+    msg["To"] = to_email
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()
+        server.login(os.getenv("MAIL_USERNAME"), os.getenv("MAIL_PASSWORD"))
+        server.sendmail(os.getenv("MAIL_USERNAME"), to_email, msg.as_string())
