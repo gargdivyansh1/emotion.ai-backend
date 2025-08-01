d5e7eed67f8b3105d841b8186a4d0a2f1bef99f0
Divyansh Garg <163748305+gargdivyansh1@users.noreply.github.com>
2025-08-01 23:58:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://emotion-ai-pi.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
